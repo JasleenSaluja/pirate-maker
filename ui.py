727adb381d9f80fc425e639fbf6af1eedd23e455
Jasleen Saluja <augjasleen29@gmail.com>
2023-05-12 01:10:52 +0530

    first commit

---
+++ ui.py
@@ -0,0 +1,19 @@
+import pygame
+
+class UI:
+    def __init__(self,surface):
+        
+        #setup
+        self.display_surface = surface
+
+        #coins collected
+        self.coins_display=pygame.image.load('graphics/ui/0.png').convert_alpha()
+        self.coin_display_rect=self.coins_display.get_rect(topleft=(45,45))
+        self.font=pygame.font.SysFont('bruno ace',30)
+
+    def show_coins(self,amount):
+        self.display_surface.blit(self.coins_display,self.coin_display_rect)
+        coin_amount_surf=self.font.render(str(amount),False,'white')
+        coin_amount_rect=coin_amount_surf.get_rect(midleft=(self.coin_display_rect.right + 4,self.coin_display_rect.centery))
+        self.display_surface.blit(coin_amount_surf,coin_amount_rect)
+        
