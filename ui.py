db3697b8b1f44e1eaa2cfb9c7bf8d150db271b06
Jasleen Saluja <augjasleen29@gmail.com>
2023-05-12 01:34:34 +0530

    pm

--- ui.py
+++ ui.py
@@ -9,11 +9,11 @@ class UI:
         #coins collected
         self.coins_display=pygame.image.load('graphics/ui/0.png').convert_alpha()
         self.coin_display_rect=self.coins_display.get_rect(topleft=(45,45))
-        self.font=pygame.font.SysFont('bruno ace',30)
+        self.font=pygame.font.SysFont('bruno ace',45)
 
     def show_coins(self,amount):
         self.display_surface.blit(self.coins_display,self.coin_display_rect)
         coin_amount_surf=self.font.render(str(amount),False,'white')
-        coin_amount_rect=coin_amount_surf.get_rect(midleft=(self.coin_display_rect.right + 4,self.coin_display_rect.centery))
+        coin_amount_rect=coin_amount_surf.get_rect(midleft=(self.coin_display_rect.right+6,self.coin_display_rect.centery +3))
         self.display_surface.blit(coin_amount_surf,coin_amount_rect)
         
